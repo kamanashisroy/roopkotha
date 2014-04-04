40dfbb2e284d6b963ef097c192f20288dae52062
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2014-04-04 23:41:06 +0600

    examining new test case for xml rendering

--- tests/guiapps/vela/shotodol.mk
+++ tests/guiapps/vela/shotodol.mk
@@ -3,7 +3,8 @@ all:
 	module -load ../../../guiapps/pad/plugin.so
 	module -load ../../../guiapps/vela/plugin.so
 	profiler
-	velapad -i velapad.txt
+	#velapad -i velapad.txt
+	velapad -i stickit.txt
 	glide
 	profiler
 

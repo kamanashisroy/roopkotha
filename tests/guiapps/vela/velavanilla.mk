fcd7a24023720ee5b2c2cc2d42cc736e9584da0d
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2014-05-27 09:56:29 +0600

    We have an about page now

--- tests/guiapps/vela/velavanilla.mk
+++ tests/guiapps/vela/velavanilla.mk
@@ -1,4 +1,4 @@
 
 all:
-	menu default <x href="velaxecute://ls" label=Open></x><x href=quit label=Quit></x><x href=aboutme label=About></x>
-	menu velaxecute://ls <x href=ls label=Open></x><x href=quit label=Quit></x><x href=aboutme label=About></x>
+	menu default <x href="velaxecute://ls" label=Open></x><x href=quit label=Quit></x><x href="velafopen://about.vapp" label=About></x>
+	menu velaxecute://ls <x href=ls label=Open></x><x href=quit label=Quit></x><x href="velafopen://about.vapp" label=About></x>

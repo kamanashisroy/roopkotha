46e44973ddeb7355833509ea23eea0c176068760
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2015-01-08 23:43:07 +0600

    We renamed turbine to spinningwheel

--- build/staticlibs.mk
+++ build/staticlibs.mk
@@ -5,7 +5,7 @@ include $(ROOPKOTHA_HOME)/$(PLATFORM)/platform_gui/staticlibs.mk
 include $(ROOPKOTHA_HOME)/libs/gui/staticlibs.mk
 include $(ROOPKOTHA_HOME)/libs/listview/staticlibs.mk
 include $(ROOPKOTHA_HOME)/libs/rtree/staticlibs.mk
-include $(SHOTODOL_HOME)/libs/turbine/staticlibs.mk
+include $(SHOTODOL_HOME)/libs/spinningwheel/staticlibs.mk
 include $(SHOTODOL_HOME)/libs/iterator/staticlibs.mk
 include $(SHOTODOL_HOME)/$(PLATFORM)/platform_fileutils/staticlibs.mk
 include $(SHOTODOL_SCRIPT_HOME)/build/staticlibs.mk
